11537e7004e721686048e22fb8a2f42229750dd3
HoVanDuc215037 <hovamduc2003bg@gmail.com>
2024-11-25 20:12:24 +0700

    sprint 1

---
+++ services/dishesService.js
@@ -0,0 +1,79 @@
+exports.processGetDishes = async () => {
+  //xample
+  return {
+    dishes: [
+      {
+        dish_id: 1,
+        dish_name: "にくまん - bánh bao",
+        dish_rate: 5.0,
+        dish_cost: 5,
+        dish_image: "/public/images/banhbao.png",
+        dish_alias: "banh-bao",
+        liked: 0
+      },
+      {
+        dish_id: 2,
+        dish_name: "バン クオン - bánh cuốn",
+        dish_rate: 5.0,
+        dish_cost: 2,
+        dish_image: "/public/images/banhcuon.png",
+        dish_alias: "banh-cuon",
+        liked: 1
+      },
+      {
+        dish_id: 3,
+        dish_name: "ブンチャー - bún chả",
+        dish_rate: 5.0,
+        dish_cost: 30,
+        dish_image: "/public/images/buncha.png",
+        dish_alias: "bun-cha",
+        liked: 1
+      },
+      {
+        dish_id: 4,
+        dish_name: "フォー - phở",
+        dish_rate: 5.0,
+        dish_cost: 3,
+        dish_image: "/public/images/pho.png",
+        dish_alias: "pho",
+        liked: 0
+      },
+      {
+        dish_id: 5,
+        dish_name: "バインセオ - bánh xèo",
+        dish_rate: 5.0,
+        dish_cost: 8,
+        dish_image: "/public/images/banhxeo.png",
+        dish_alias: "banh-xeo",
+        liked: 1
+      },
+      {
+        dish_id: 6,
+        dish_name: "ゴーイ　カー - gỏi cá",
+        dish_rate: 4.8,
+        dish_cost: 12,
+        dish_image: "/public/images/goica.png",
+        dish_alias: "goi-ca",
+        liked: 0
+      },
+      {
+        dish_id: 7,
+        dish_name: "ブンダウ	 - bún đậu",
+        dish_rate: 4.8,
+        dish_cost: 7,
+        dish_image: "/public/images/bundau.png",
+        dish_alias: "bun-dau",
+        liked: 0
+      },
+      {
+        dish_id: 8,
+        dish_name: "甘酸(あまず)っぱい豚(ぶた)カルビ	 - sườn xào chua ngọt",
+        dish_rate: 4.8,
+        dish_cost: 10,
+        dish_image: "/public/images/bundau.png",
+        dish_alias: "bun-dau",
+        liked: 0
+      }
+    ]
+  }
+}
